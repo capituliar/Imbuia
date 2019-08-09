ce0c7028e33396dcdc0c22e5d9277ffcf6374cc9
Samara Silva <capituliar@gmail.com>
2019-08-09 13:54:57 -0300

    Correções no envio de e-mail

--- contato.php
+++ contato.php
@@ -1,27 +1,22 @@
-
 <?php
 
     if (isset ($_POST['email']) && !empty($_POST['email'])) {
 
-    $nome = addcslashes($_POST['name']);
-    $email = addcslashes($_POST['email']);
-    $mensagem = addcslashes($_POST['message']);
-    
-    $to = "sams2723@gmail.com";
+    $to = "cursosimbuia@gmail.com";
     $subject = "Contato - Cursos Imbuia";
-    $body = "Nome: ".$nome. "\r\n"
-            ."Email ".$email. "\r\n"
-            ."Mensagem: ".$mensagem;
-    $header = "From: samara@cursosimbuia.com"."\r\n"
-            ."Replay-To: ".$email."\r\n"
+    $body = "Nome: ".$_POST['name']. "\r\n"
+            ."Email: ".$_POST['email']. "\r\n"
+            ."Mensagem: ".$_POST['message'];
+    $header = "From: cursosimbuia@gmail.com"."\r\n"
+            ."Replay-To: ".$_POST['email']."\r\n"
             ."X=Mailer:PHP".phpversion();
 
     if (mail ($to, $subject,$body,$header)) {
-        echo ("Email enviado com sucesso!");
+        echo '<script>alert("Mensagem enviada");</script>';
     } else {
-        echo ("Falha. O email não pode ser enviado.");
+        echo '<script>alert("Falha no envio. Por favor, envie a mensagem diretamente para cursosimbuia@gmail.com");</script>';
     }
-
-
-}
+}  
+header("Refresh: 0; index.html");
+  
 ?>
